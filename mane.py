eb03677e53665f1165fe7858b553d48e279a4e04
win10 <legioner9@inbox.ru>
2021-11-19 12:07:25 +0700

    <> #2

---
+++ mane.py
@@ -0,0 +1,17 @@
+print("HW")
+a = False
+s = 'fvdsdfv'
+d = 0
+# l: list[int, str] = [2, 4, 'rw']
+
+# if 2 > 3:
+#     print('2>3')
+# elif 5 > 4:
+#     print('4 > 3')
+
+while d < 5:
+    print(d)
+    d = d + 1
+
+for i in range(1, 3):
+    print(i)
